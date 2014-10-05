b69755336b9ac7a26637e1e8e34a312a027b449c
jnath <jonathan.faivre@gmail.com>
2014-10-05 18:47:11 +0200

    positionning img

--- data/js/main.js
+++ data/js/main.js
@@ -1,14 +1,75 @@
-if (window.jQuery) { console.log("JQuery loaded"); } else { console.log("JQuery not loaded");}
+var c = new Center();
 
-var c = new centrage();
+function initAndResize(){
+	c.centerY("navigation",-200);
+}
+
+function initClickAndScroll(){
+	$('a[href^="#"]').click(function(){  
+	    var the_id = $(this).attr("href");  
+	  
+	    $('html, body').animate({  
+	        scrollTop:$(the_id).offset().top  
+	    }, 'slow');  
+	    return false;  
+	});
+}
+
+function initPositionImg(){
+	var texts = $('#flux_textes').children();
+	var leftPosImg = $('#navigation').offset().left + $('#navigation').outerHeight(true);
+	var i=0;
+	var leftTotal=0;
+	$('#flux_images img').each(function(){
+		var containerWidth = $(texts[i]).outerHeight()
+		
+		var imgWith = containerWidth - $(texts[i]).outerHeight();
+		var colonne = $(texts[i]).find('.colonnes')[0];
+		var moreLeft = 0;
+
+		leftTotal+=$(texts[i]).offset().top;
+
+		if($(colonne).hasClass('prefix-1')){
+			moreLeft =  ( ( $(this).outerHeight()-imgWith ) /2 ) + $(colonne).outerHeight();
+		}else if($(colonne).hasClass('prefix-2')){
+			moreLeft = ( $(this).outerHeight()-imgWith ) /2 ;
+		}
+		// TODO: add for prfix-3
+	    $(this).css({
+	        position: "absolute",
+	        top: $(texts[i]).offset().top + ($(texts[i]).outerHeight()-$(this).outerHeight()),
+	        left: leftTotal + moreLeft,
+	    });
+	    i++;
+	});
+}
 
 $( document ).ready(function() {
-	c.ajouterElement('#flux_images');
-	c.ajouterElement('#navigation');
+	initAndResize();
+	initClickAndScroll();
 
-	c.run();
-	setTimeout('c.run()',100);
-	setTimeout('c.run()',200); //delay pour safari et chrome…
+	var leftPosImg = $('#navigation').offset().left + $('#navigation').outerHeight(true);
+
+	$("html").niceScroll();
+
+	$('#flux_images').css({
+	    position: "absolute",
+	    "z-index": -9998,
+	    left: leftPosImg,
+	    top: 0,
+	    "pointer-events": "none"
+	});
+	
+	initPositionImg();
+
+	$( window ).scroll(function() {
+	    $('#flux_images')
+	    .animate({ 
+	        left: leftPosImg - ( $(this).scrollTop() )
+	    }, 0.5, 'swing');
+	});
+
+	$( window ).resize(function() {
+		initAndResize();
+	});
 });
-
-console.log('loaded : main.js');
